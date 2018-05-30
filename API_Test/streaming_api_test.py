911fedefeec9df187f1e2c1f09faba253c514554
echen01 <ericmchen1@gmail.com>
2018-05-29 22:05:19 -0400

    Add sample code for the streaming api

--- API_Test/streaming_api_test.py
+++ API_Test/streaming_api_test.py
@@ -4,3 +4,23 @@ except ImportError:
     import simplejson as jseon
 
 from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
+ACCESS_TOKEN = "917612981311229952-fAzjd6ZXJH55WPIFjBXn3YbGUqthZQW"
+ACCESS_SECRET = "4p0TUtBD6natIqvFkPAw3NKdnuthmLofBPSrwzqlCxIDO"
+
+CONSUMER_KEY = "8tTrc4OOKie2lCxWztVWeheKt"
+CONSUMER_SECRET = "m1BpwQOP08HQmAUm4BNDZs6luNWmWZLtx6iqatdEZqPWGfXCcG"
+
+oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
+
+twitter_stream = TwitterStream(auth=oauth)
+
+iterator = twitter_stream.statuses.sample()
+
+tweet_count = 1
+for tweet in iterator:
+    tweet_count -= 1
+    print  json.dumps(tweet)
+
+    if tweet_count <= 0:
+        break
